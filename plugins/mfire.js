6577cf5d7decbeb3db2a552ab7feb28f48f9a85a
✰✶  DARK KILLER ✰✶ <99461944+DarkKiller-Official@users.noreply.github.com>
2025-01-04 17:46:55 +0530

    Update mfire.js

--- plugins/mfire.js
+++ plugins/mfire.js
@@ -3,7 +3,6 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const { cmd } = require('../command');
 
-
 cmd({
     pattern: "mfire",
     desc: "Download a file from MediaFire",
@@ -12,35 +11,38 @@ cmd({
     filename: __filename
 }, async (conn, mek, m, { from, quoted, reply }) => {
     try {
-        const mediafireUrl = m.text.split(" ")[1]; 
+        const mediafireUrl = m.text.split(" ")[1];
 
         if (!mediafireUrl) {
             return reply("Please provide a MediaFire URL.");
         }
 
-    
+       
         const { data } = await axios.get(mediafireUrl);
         const $ = cheerio.load(data);
 
-        
+       
         const downloadLink = $('a#download_button').attr('href');
+        console.log("Download link extracted:", downloadLink);
 
         if (!downloadLink) {
             return reply("Could not find the download link. Please check the URL.");
         }
 
-      
+    
         const response = await axios.get(downloadLink, { responseType: 'stream' });
+        console.log("File download started...");
 
-        const fileName = mediafireUrl.split('/').pop(); 
+        const fileName = mediafireUrl.split('/').pop();
         const filePath = `./downloads/${fileName}`;
 
-   
+     
         const writer = fs.createWriteStream(filePath);
         response.data.pipe(writer);
 
         writer.on('finish', () => {
-       
+            console.log("File downloaded successfully.");
+         
             conn.sendMessage(from, {
                 document: { url: filePath },
                 fileName: fileName,
@@ -50,12 +52,12 @@ cmd({
         });
 
         writer.on('error', (err) => {
-            console.error(err);
+            console.error("Error saving file:", err);
             reply("Error downloading the file.");
         });
 
     } catch (error) {
-        console.error(error);
-        reply("An error occurred while downloading the file.");
+        console.error("An error occurred:", error);
+        reply(`An error occurred while downloading the file: ${error.message}`);
     }
 });
