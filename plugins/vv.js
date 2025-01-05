67451b2094882504bede8d02d97c144987965745
itsme-didulabot <ddidula034@gmail.com>
2025-01-05 07:24:57 +0530

    Update vv.js

--- plugins/vv.js
+++ plugins/vv.js
@@ -1,5 +1,7 @@
-const { cmd, commands } = require('../command');
-const config = require('../config');
+const {
+  downloadContentFromMessage
+} = require('@whiskeysockets/baileys')
+
 cmd({
     pattern: "vv",
     react: "👀",
@@ -37,13 +39,13 @@ if(m.quoted.mtype === "viewOnceMessage")
  if(m.quoted.message.imageMessage )
 { 
   let cap = m.quoted.message.imageMessage.caption;
-  let anu = await conn.downloadAndSaveMediaMessage(citel.quoted.message.imageMessage)
+  let anu = await conn.downloadAndSaveMediaMessage(m.quoted.message.imageMessage)
   conn.sendMessage(m.chat,{image:{url : anu},caption : cap })
 }
 else if(m.quoted.message.videoMessage )
 {
   let cap = m.quoted.message.videoMessage.caption;
-  let anu = await conn.downloadAndSaveMediaMessage(citel.quoted.message.videoMessage)
+  let anu = await conn.downloadAndSaveMediaMessage(m.quoted.message.videoMessage)
   conn.sendMessage(m.chat,{video:{url : anu},caption : cap })
 }
 
