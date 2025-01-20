fb3508a32ce348b46f4fb181b63229df15e63b7a
itsme-didulabot <ddidula034@gmail.com>
2025-01-20 15:49:36 +0530

    Update other.js

--- plugins/other.js
+++ plugins/other.js
@@ -8,6 +8,30 @@ const { fetchJson, getBuffer } = require('../lib/functions');
 
 
 cmd({
+    pattern: "dog",
+    desc: "Fetch a random dog image.",
+    category: "other",
+    react: "🐶",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const apiUrl = `https://dog.ceo/api/breeds/image/random`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '*◆─〈 ✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦ 〉─◆*' }, { quoted: mek });
+    } catch (e) {
+        console.log(e);
+        reply(`Error fetching dog image: ${e.message}`);
+    }
+});
+
+
+
+
+
+cmd({
     pattern: "animeboy",
     desc: "Fetch a random anime boy image.",
     category: "other",
