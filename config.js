e57ed532832381797f6c3161b98cd6fb3b3103a5
itsme-didulabot <ddidula034@gmail.com>
2025-01-20 11:49:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ FOOTER: process.env.FOOTER || "Didula MD V2 💚",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "uURmxaaZ#NPLl5UuXqkTyR6LXeCH14GqY9V3OCFw7PNlegZxKCeE",
 };
 
 
