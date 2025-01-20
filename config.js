72cfa1ebe3e9b3da7262235ec1cfa754c48c7bf2
itsme-didulabot <ddidula034@gmail.com>
2025-01-20 12:58:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 STATUS_READ_MSG: process.env.STATUS_READ_MSG || "Didula MD V2 💚",
 ANTI_DELETE: process.env.ANTI_DELETE || "true",
-FOOTER: process.env.FOOTER || "Didula MD V2 💚",
+FOOTER: process.env.FOOTER || "*◆─〈 ✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦ 〉─◆*",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
 IMAGE_LIMIT: process.env.IMAGE_LIMIT || "3",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
