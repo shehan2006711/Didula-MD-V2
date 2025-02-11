eb92f633b8f541d2d8f0b575e50d328dd47a1437
itsme-didulabot <ddidula034@gmail.com>
2025-02-11 18:46:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -270,6 +270,57 @@ mek.type === "stickerMessage"
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
 //============================================================================ 
+    //============================================================================ 
+    if(body === "send" || body === "Send" || body === "Ewpm" || body === "ewpn" || body === "Dapan" || body === "dapan" || body === "oni" || body === "Oni" || body === "save" || body === "Save" || body === "ewanna" || body === "Ewanna" || body === "ewam" || body === "Ewam" || body === "sv" || body === "Sv"|| body === "දාන්න"|| body === "එවම්න"){
+        // if(!m.quoted) return reply("*Please Mention status*")
+        const data = JSON.stringify(mek.message, null, 2);
+        const jsonData = JSON.parse(data);
+        const isStatus = jsonData.extendedTextMessage.contextInfo.remoteJid;
+        if(!isStatus) return
+
+        const getExtension = (buffer) => {
+            const magicNumbers = {
+                jpg: 'ffd8ffe0',
+                png: '89504e47',
+                mp4: '00000018',
+            };
+            const magic = buffer.toString('hex', 0, 4);
+            return Object.keys(magicNumbers).find(key => magicNumbers[key] === magic);
+        };
+
+        if(m.quoted.type === 'imageMessage') {
+            var nameJpg = getRandom('');
+            let buff = await m.quoted.download(nameJpg);
+            let ext = getExtension(buff);
+            await fs.promises.writeFile("./" + ext, buff);
+            const caption = m.quoted.imageMessage.caption;
+            await conn.sendMessage(from, { image: fs.readFileSync("./" + ext), caption: caption });
+        } else if(m.quoted.type === 'videoMessage') {
+            var nameJpg = getRandom('');
+            let buff = await m.quoted.download(nameJpg);
+            let ext = getExtension(buff);
+            await fs.promises.writeFile("./" + ext, buff);
+            const caption = m.quoted.videoMessage.caption;
+            let buttonMessage = {
+                video: fs.readFileSync("./" + ext),
+                mimetype: "video/mp4",
+                fileName: `${m.id}.mp4`,
+                caption: "> ✨ᴅɪᴅᴜʟᴀ ᴍᴅ - " + caption ,
+                headerType: 4
+            };
+            await conn.sendMessage(from, buttonMessage,{
+                quoted: mek
+            });
+        }
+       }
+
+
+
+
+
+//============================================================================ 
+
+
 
    
 })
