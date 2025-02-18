96ea11325e7f78c51baa8124175a156f676da640
shehan2006711 <154323632+shehan2006711@users.noreply.github.com>
2025-02-18 13:29:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,11 +8,11 @@ module.exports = {
 
 FOOTER: process.env.FOOTER || "*◆─〈 ✦𝐃𝐢𝐝𝐮𝐥𝐚 𝐌𝐃 𝐕𝟐✦ 〉─◆*",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tC37Q7B/20241220-122443.jpg",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
 MODE: process.env.MODE || "public",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
-AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
 AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*SEEN YOUR STATUS BY Didula MD V2*",
 ANTI_CALL: process.env.ANTI_CALL || "false",
@@ -22,7 +22,7 @@ ANTI_CALL: process.env.ANTI_CALL || "false",
 
 
 
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~86sHFALT#fcVgFd3u0z7THcqRsXJlmfwbtv-gkB-XSZy7T0Vwr-c",
 
 
 
